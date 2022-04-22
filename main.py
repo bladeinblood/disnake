163c2ad6428fd5ed221c6c088bd55ee438c37aa6
bladeinblood <101563891+bladeinblood@users.noreply.github.com>
2022-04-22 18:54:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import disnake
+from disnake.ext import commands
+
+PREFIX = '!'
+
+client = commands.Bot(command_prefix = PREFIX, sync_commands = False)
+
+@client.slash_command(description = "Hello bro)")
+async def hello(ctx, member: disnake.Member):
+  await ctx.send(f'Hi {member.mention}')
+  
+@client.slash_command(description = "Help for you)")
+async def help(ctx):
+  await ctx.send('команды')
+  
